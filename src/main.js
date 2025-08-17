0a603d7b8ce6fe741fb48caa5941781b72c6628c
danishhhirfannn <danishirfan.workspace@gmail.com>
2025-08-17 10:43:02 +0800

    Volt PrimeVue Initialisation

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import PrimeVue from 'primevue/config';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(PrimeVue, {
+    unstyled: true
+});
+
+app.mount('#app')
